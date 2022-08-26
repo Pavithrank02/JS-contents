ef9e1153a089e292af91e8f8bdd98f74bce892dc
Pavithran <pavithrank02@gmail.com>
2022-08-26 07:43:26 +0530

    render

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,10 +30,15 @@ parentEl.insertAdjacentHTML('afterbegin', markup);
 const showRecipe = async function() {
   //loading recipe
   try {
+
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if(!id) return;
     // 1 loading recipe
     renderSpinner(recipeContainer);
-    const res = await fetch(//'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca57'
-    'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    const res = await fetch(
+    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
     if(!res.ok) throw new Error(`${data.message}(${res.status})`)
@@ -146,4 +151,9 @@ const showRecipe = async function() {
     alert(err)
   }
 }
-showRecipe();
+//showRecipe();
+
+// ['hashchange', 'load'].forEach(ev => window.addEventListener(ev,showRecipe))
+
+window.addEventListener('hashchange',showRecipe);
+window.addEventListener('load',showRecipe);
